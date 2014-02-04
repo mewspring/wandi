4d69be63b6edda9ce54995bcf85f297e7739829c
mewmew <rnd0x00@gmail.com>
2014-02-04 16:36:37 +0100

    wde: Provide direct link to GoDoc for github.com/mewmew/we.

--- event.go
+++ event.go
@@ -1,5 +1,5 @@
 package wde
 
 // An Event represents a window event. The various event types are defined at:
-//    github.com/mewmew/we
+//    http://godoc.org/github.com/mewmew/we
 type Event interface{}
