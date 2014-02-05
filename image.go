7b7744585262672e97e644b6abaeb08980c0cde0
mewmew <rnd0x00@gmail.com>
2014-02-05 20:21:00 +0100

    wandi: Add Width and Height methods to the Image interface.

--- image.go
+++ image.go
@@ -6,6 +6,10 @@ import (
 
 // An Image is a mutable collection of pixels.
 type Image interface {
+	// Width returns the width of the image.
+	Width() int
+	// Height returns the height of the image.
+	Height() int
 	// Draw draws the entire src image onto the dst image starting at the
 	// destination point dp.
 	Draw(dp image.Point, src Image) (err error)
