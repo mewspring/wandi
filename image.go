9bbfbb62644098eb9e3264342bf748d9be8ec076
mewmew <rnd0x00@gmail.com>
2014-02-06 22:15:03 +0100

    wandi: Change the definition of DrawRect to make it more consistent with Draw.

--- image.go
+++ image.go
@@ -13,7 +13,7 @@ type Image interface {
 	// Draw draws the entire src image onto the dst image starting at the
 	// destination point dp.
 	Draw(dp image.Point, src Image) (err error)
-	// DrawRect fills the destination rectangle dr of the dst image with
-	// corresponding pixels from the src image starting at the source point sp.
-	DrawRect(dr image.Rectangle, src Image, sp image.Point) (err error)
+	// DrawRect draws a subset of the src image, as defined by the source
+	// rectangle sr, onto the dst image starting at the destination point dp.
+	DrawRect(dp image.Point, src Image, sr image.Rectangle) (err error)
 }
